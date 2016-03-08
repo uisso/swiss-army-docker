459b36b8db4d834072634fa045e507f0fa29b6a8
Wilson Almeida <wilson.almeida@neoway.com.br>
2016-03-08 09:52:53 -0300

    docker image as parameter

--- library/mongo.go
+++ library/mongo.go
@@ -6,17 +6,13 @@ import (
 	"github.com/uisso/swiss-army-docker/util"
 )
 
-const (
-	mongoImage = "library/mongo"
-)
-
 // SetupMongoContainer sets up a real MongoDB instance for testing purposes,
 // using a Docker container. It returns the container ID and its IP address,
 // or makes the test fail on error.
 // Currently using https://hub.docker.com/_/mongo/
-func SetupMongoContainer(t *testing.T) (c util.ContainerID, ip string) {
+func SetupMongoContainer(t *testing.T, image string) (c util.ContainerID, ip string) {
 	start := func() (string, error) {
-		return util.Run("-d", "-p", "27017:27017", "--name", "army_leaf", mongoImage)
+		return util.Run("-d", "-p", "27017:27017", "--name", "army_leaf", image)
 	}
-	return util.SetupContainer(t, mongoImage, 27017, 10 * time.Second, start)
+	return util.SetupContainer(t, image, 27017, 10 * time.Second, start)
 }
--- library/rabbit.go
+++ library/rabbit.go
@@ -6,16 +6,13 @@ import (
 	"time"
 )
 
-const (
-	rabbitImage = "registry-aws.neoway.com.br/prevention/rabbitmq:3.5.6"
-)
 
 // SetupRabbitContainer sets up a real Rabbit instance for testing purposes,
 // using a Docker container. It returns the container ID and its IP address,
 // or makes the test fail on error.
-func SetupRabbitContainer(t *testing.T) (c util.ContainerID, ip string) {
+func SetupRabbitContainer(t *testing.T, image string) (c util.ContainerID, ip string) {
 	start := func() (string, error) {
-		return util.Run("-d", "-p", "5672:5672", "--name", "army_bugsy", rabbitImage)
+		return util.Run("-d", "-p", "5672:5672", "--name", "army_bugsy", image)
 	}
-	return util.SetupContainer(t, rabbitImage, 5672, 10 * time.Second, start)
+	return util.SetupContainer(t, image, 5672, 10 * time.Second, start)
 }
